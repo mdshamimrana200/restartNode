4e23166dda58ef37020713528a2aa63ee36736c4
mdshamimrana200 <mdshamimrana20@gmail.com>
2024-03-29 17:14:00 +0600

    up

--- routers/userRouters.js
+++ routers/userRouters.js
@@ -1,11 +1,16 @@
-const { getAllUser,getOneUser,updateUser,deleteUser,creatUser } = require("../controllers/usercontrollers")
+const router = require("express").Router();
+const {
+  getAllUser,
+  getOneUser,
+  updateUser,
+  deleteUser,
+  creatUser,
+} = require("../controllers/usercontrollers");
 
-const router = require("express").Router()
+router.get("/", getAllUser);
+router.get("/:name", getOneUser);
+router.post("/", creatUser);
+router.put("/:name", updateUser);
+router.delete("/", deleteUser);
 
-router.get("/",getAllUser)
-router.get("/:name",getOneUser)
-router.post("/",creatUser)
-router.put("/:name",updateUser)
-router.delete("/",deleteUser)
-
-module.exports = router
+module.exports = router;
